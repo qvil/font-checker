a6742fc05d6047453891618d7f39a9adf1f11163
Taesu Hyeon <qvil1127@gmail.com>
2019-03-04 02:35:36 +0900

    Updates

---
+++ precache-manifest.aa3eca5f1e8156b4635dba0b4464b719.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/font-checker/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2df26a53acbbc75d1880",
+    "url": "/font-checker/static/js/runtime~main.2df26a53.js"
+  },
+  {
+    "revision": "8c6691f61212c15ecac4",
+    "url": "/font-checker/static/js/main.8c6691f6.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/font-checker/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "8c6691f61212c15ecac4",
+    "url": "/font-checker/static/css/main.c16fc9c3.chunk.css"
+  },
+  {
+    "revision": "e667283aad4833f560a2f6928c632e42",
+    "url": "/font-checker/index.html"
+  }
+];
