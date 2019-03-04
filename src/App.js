90abc0370a8a8726d2376a344355197f83d5930c
Taesu Hyeon <qvil1127@gmail.com>
2019-03-05 02:06:23 +0900

    Add styled-components, TextArea handling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const TextArea = styled.textarea`
+  outline: none;
+`;
+
+const App = () => {
+  const [text, setText] = useState("무궁화 꽃이 활짝 피었습니다.");
+
+  return (
+    <div>
+      <h1>Font Checker</h1>
+      <select>
+        <option value="1">1</option>
+        <option value="2">2</option>
+      </select>
+      <TextArea value={text} onChange={e => setText(e.target.value)} />
+    </div>
+  );
+};
 
 export default App;
